e56a052b8d00467c993a6caef3a14f9acb076e6a
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-12-30 01:12:38 +0000

    feature 1 work in progress. Header component completed.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import HelloWorld from '../components/hello-world';
+import Header from '../components/header';
+import Background from '../components/background';
 
 export default function Home(props) {
   return (
     <div>
-      <HelloWorld />
+      <Header />
+      <Background />
     </div>
   );
 }
