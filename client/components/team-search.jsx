832cb37ff495c56ec3d2c57c4856bbf322c95bb2
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-12-31 00:12:43 +0000

    99% complete with issue-1

--- client/components/team-search.jsx
+++ client/components/team-search.jsx
@@ -1,23 +1,72 @@
 import React from 'react';
 
-export default function TeamSearch(props) {
-  return (
-    <div className="gray-background">
-      <form className="team-search-form">
-        <div className="row">
-          <div className="col-100 row height-70 ai-center jc-center relative">
-            <input type="text" className="team-search-bar" id="team-search-input" placeholder="Search for team (e.g. 'Real Madrid')" required />
-            <button type="submit" className="team-search-button">
-              <i className="fa-solid fa-magnifying-glass" />
-            </button>
+export default class TeamSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTeamSearchChange = this.handleTeamSearchChange.bind(this);
+    this.state = {
+      teamsearch: '',
+      view: '',
+      response: []
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/teamsearch/${this.state.teamsearch}`, {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ response: data });
+        this.setState({ view: 'team search' });
+      })
+      .catch(err => console.error('Fetch Failed!', err));
+  }
+
+  handleTeamSearchChange(event) {
+    this.setState({ teamsearch: event.target.value });
+  }
+
+  render() {
+    if (this.state.view === '') {
+      return (
+        <div className="gray-background">
+          <form className="team-search-form" onSubmit={this.handleSubmit}>
+            <div className="row">
+              <div className="col-100 row height-70 ai-center jc-center relative">
+                <input name="teamsearch" value={this.state.teamsearch} type="text" className="team-search-bar" id="team-search-input" placeholder="Search for team (e.g. 'Real Madrid')" onChange={this.handleTeamSearchChange} required />
+                <button type="submit" className="team-search-button">
+                  <i className="fa-solid fa-magnifying-glass" />
+                </button>
+              </div>
+            </div>
+            <div className="row height-30">
+              <div className="col-100 row fifa-logo jc-center">
+                <img src="/images/fifa-logo.jpg" className="fifa-logo-pic" />
+              </div>
+            </div>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div className="gray-background">
+          <div className="row jc-center">
+            <div className="col-100-40">
+              <img className="team-image" src={this.state.response.response[0].team.logo} alt="pic of team logo" />
+              <p className="team-info text-align-center">{this.state.response.response[0].team.name} ({this.state.response.response[0].team.code}) | {this.state.response.response[0].team.country} | Est.{this.state.response.response[0].team.founded}</p>
+            </div>
+          </div>
+          <div className="row jc-center">
+            <div className="col-100-40">
+              <img className="stadium-image" src={this.state.response.response[0].venue.image} alt="pic of stadium" />
+              <p className="team-info text-align-center">{this.state.response.response[0].venue.name} | {this.state.response.response[0].venue.city} | {this.state.response.response[0].venue.capacity}</p>
+            </div>
           </div>
         </div>
-        <div className="row height-30">
-          <div className="col-100 row fifa-logo jc-center">
-            <img src="/images/fifa-logo.jpg" className="fifa-logo-pic" />
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+      );
+    }
+  }
 }
--- server/index.js
+++ server/index.js
@@ -2,13 +2,24 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+const jsonMiddleware = express.json();
 
 const app = express();
 
 app.use(staticMiddleware);
+app.use(jsonMiddleware);
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.get('/api/teamsearch/:teamname', (req, res) => {
+  fetch(`https://api-football-v1.p.rapidapi.com/v3/teams?search=${req.params.teamname}`, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '3e6df72b18msh42bd29bf1a7c124p1c6cfbjsn347a96b426a4',
+      'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+    }
+  })
+    .then(response => response.json())
+    .then(data => res.status(200).json(data))
+    .catch(err => console.error('Fetch Failed!', err));
 });
 
 app.use(errorMiddleware);
