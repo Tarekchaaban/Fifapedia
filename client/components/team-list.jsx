18697a2d646f2ea3b94dc48ad85a35d020f2b367
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2023-01-04 05:38:30 +0000

    issue 3 work in progress, half way complete.

--- client/components/team-list.jsx
+++ client/components/team-list.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 
 export default class TeamList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleXClick = this.handleXClick.bind(this);
+    this.createModal = this.createModal.bind(this);
+    this.handleCancelClick = this.handleCancelClick.bind(this);
+    this.state = {
+      isDeleting: false
+    };
+  }
+
+  handleXClick(event) {
+    this.setState({ isDeleting: true });
+  }
+
+  handleCancelClick(event) {
+    this.setState({ isDeleting: false });
+  }
+
+  createModal() {
+    return (
+      <div>
+        <div className="overlay" />
+        <div className="modal">
+          <div className="row">
+            <div className="column-full">
+              <h2 className="modal-text-format">Are you sure you want to delete this team?</h2>
+            </div>
+          </div>
+          <div className="row jc-center ai-center">
+            <div className="column-full">
+              <div className="row">
+                <div className="col-50 text-align-center">
+                  <button className="no-button" onClick={this.handleCancelClick}>Cancel</button>
+                </div>
+                <div className="col-50 text-align-center">
+                  <button className="yes-button">Confirm</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   render() {
-    if (this.props.view === 'team list') {
+    const modal = this.createModal();
+    if (this.props.view === 'team list' && this.state.isDeleting === false) {
       return (
         <div>
           <h1 className="team-list-header">Teams</h1>
@@ -11,8 +56,11 @@ export default class TeamList extends React.Component {
             {
             this.props.teamlist.map(team => {
               return (
-                <li className="col-100-50" key={team.teamId}>
-                  <div className="list-blue-background">
+                <li className="col-100-50" key={team.entryId} id={team.teamId}>
+                  <div className="list-blue-background relative">
+                    <button className="x-mark-button">
+                      <i className="fa-solid fa-xmark" onClick={this.handleXClick} />
+                    </button>
                     <div className="row jc-center">
                       <div className="column-one-sixth">
                         <img className="team-list-logo" src={team.crestUrl} alt='team logo' />
@@ -26,6 +74,36 @@ export default class TeamList extends React.Component {
               );
             })
           }
+          </ul>
+        </div>
+      );
+    } else if (this.props.view === 'team list' && this.state.isDeleting === true) {
+      return (
+        <div>
+          {modal}
+          <h1 className="team-list-header">Teams</h1>
+          <ul className="list-group shadow-sm row wrapped">
+            {
+              this.props.teamlist.map(team => {
+                return (
+                  <li className="col-100-50" key={team.entryId} id={team.teamId}>
+                    <div className="list-blue-background relative">
+                      <button className="x-mark-button">
+                        <i className="fa-solid fa-xmark" onClick={this.handleXClick} />
+                      </button>
+                      <div className="row jc-center">
+                        <div className="column-one-sixth">
+                          <img className="team-list-logo" src={team.crestUrl} alt='team logo' />
+                        </div>
+                        <div className="column-two-thirds">
+                          <p className="team-list-name">{team.teamName}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })
+            }
           </ul>
         </div>
       );
